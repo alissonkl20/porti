34d6016f3c03c1a0522d3c9c84e941f8a3cda911
Alisson Almeida de Oliveira <contato@alissonalmeida.com>
2025-12-13 19:47:24 -0300

    corrige a formatação do objeto proxy no arquivo de configuração do Vite

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: 
+    proxy: {
       '/api': {
         target: process.env.VITE_API_URL || 'http://localhost:3001',
         changeOrigin: true,
