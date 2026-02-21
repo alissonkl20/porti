19e2a4b01fbf40414ec6e985d2fa19f03c4fe0b5
Alisson Almeida de Oliveira <alissonttfire12@gmail.com>
2026-02-21 02:09:56 -0300

    up

--- app/src/components/animation/Android.jsx
+++ app/src/components/animation/Android.jsx
@@ -3,9 +3,9 @@ import '../../css/Android.css';
 export function Android() {
     return (
         <div className="devices">
-             <div>
-                <h1>Conectados globalmente</h1>
-             </div>
+                 <div>
+                     <h1 className='liff'>Globally connected, cross-platform</h1>
+                 </div>
             <div className="iphone" aria-label="iPhone 16 template">
                 <div className="iphone-notch" />
                 <div className="iphone-screen">
